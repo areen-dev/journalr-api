47bdaebf84910a7262cae1e70d8ef20b62cf05a6
Areen <1coding0areen.js@gmail.com>
2026-05-08 21:42:54 +0530

    add POST /entries route to create new entries

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from storage import load_entries, save_entries
 
 app = FastAPI()
 
 
+class Entry(BaseModel):
+    content: str
+
+
 @app.get("/entries")
-def my_func():
+def get_function():
     return load_entries()
+
+
+@app.post("/entries")
+def post_function(entry: Entry):
+    entries = load_entries()
+    new_entries = {"id": len(entries) + 1, "content": entry.content}
+    entries.append(new_entries)
+    save_entries(entries)
+    return new_entries
