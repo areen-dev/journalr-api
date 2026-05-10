b6d4c6724727895e7d539193831f333cb0a07ac7
Areen <1coding0areen.js@gmail.com>
2026-05-10 16:38:00 +0530

    add DELETE /entries route to delete entries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from storage import load_entries, save_entries
@@ -22,3 +22,16 @@ def post_function(entry: Entry):
     entries.append(new_entries)
     save_entries(entries)
     return new_entries
+
+
+@app.delete("/entries/{id}")
+def delete_function(id: int):
+    entries = load_entries()
+    for entry in entries:
+        if id == entry["id"]:
+            entries.remove(entry)
+            break
+    else:
+        raise HTTPException(status_code=404, detail="Entry not found")
+    save_entries(entries)
+    return entries
