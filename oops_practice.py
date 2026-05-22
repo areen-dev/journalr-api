c06108f12e81a5c2f5273e4f8d2e52c2695bf44b
areen-dev <areen.root@gmail.com>
2026-05-22 20:30:01 +0530

    oops_practice: add type hints

--- oops_practice.py
+++ oops_practice.py
@@ -1,3 +1,6 @@
 class JournalEntry:
-    def __init__(self, content):
-        self.contnet = content
+    def __init__(self, content: str):
+        self.content = content
+
+    def preview(self) -> str:
+        return self.content[0:50]
